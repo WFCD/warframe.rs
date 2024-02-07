f521b2b36c2766d4089cb96f3f5ddfbfa35e9d5d
Mettwasser <kaicrafter2000@gmail.com>
2024-02-07 20:46:33 +0100

    docs: Fix typo in on_fissure_update

--- warframe.rs/examples/listener_array/src/main.rs
+++ warframe.rs/examples/listener_array/src/main.rs
@@ -4,7 +4,7 @@ use warframe::worldstate::prelude::*;
 /// Used to "listen" to updates of a Type T that implements `RTArray`.
 /// The function gets passed `t: Change<'_, T>`
 #[warframe::worldstate::listen_any(Fissure)]
-async fn on_cetus_update(fissure: Change<'_, Fissure>) {
+async fn on_fissure_update(fissure: Change<'_, Fissure>) {
     match fissure {
         // Change<T> is an enum representing whether an item was added or removed
         Change::Added(fissure) => println!("A Fissure was just ADDED: {:?}", fissure),
@@ -17,6 +17,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_builder().filter_level(log::LevelFilter::Debug).init();
 
     let client = Client::new();
-    on_cetus_update(&client).await.unwrap();
+    on_fissure_update(&client).await.unwrap();
     Ok(())
 }
