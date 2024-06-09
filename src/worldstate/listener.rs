6e96783cbc39d3bd9c3a16764e01ce8f37b2a270
Mettwasser <kaicrafter2000@gmail.com>
2024-06-09 22:09:35 +0200

    fix: listeners are now properly spawnable in a tokio::task::spawn(f)

--- src/worldstate/listener.rs
+++ src/worldstate/listener.rs
@@ -1,6 +1,6 @@
 use std::future::Future;
 
-use super::models::{Endpoint, Model, RTArray, RTObject, TimedEvent};
+use super::models::{Endpoint, Model, RTArray, RTObject};
 
 pub enum Change<'a, T> {
     Added(&'a T),
@@ -8,39 +8,42 @@ pub enum Change<'a, T> {
 }
 
 // ----------
-pub trait ListenerCallback<'a, T, S = ()>
+pub trait ListenerCallback<'a, T>
 where
-    T: Sized + Endpoint + RTObject + Model + 'a,
-    S: Sized + Send + Sync,
+    T: Sized + Endpoint + RTObject + Model,
 {
-    fn call(&self, before: &'a T, after: &'a T) -> impl Future + 'a;
+    type Fut: Future + Send;
+    fn call(&self, before: &'a T, after: &'a T) -> Self::Fut;
 }
 
 impl<'a, T, Fut, Func> ListenerCallback<'a, T> for Func
 where
     T: Sized + Endpoint + RTObject + Model + 'a,
-    Fut: Future + 'a,
+    Fut: Future + Send,
     Func: Fn(&'a T, &'a T) -> Fut,
 {
-    fn call(&self, before: &'a T, after: &'a T) -> impl Future + 'a {
+    type Fut = Fut;
+    fn call(&self, before: &'a T, after: &'a T) -> Self::Fut {
         self(before, after)
     }
 }
 
 pub trait NestedListenerCallback<'a, T>
 where
-    T: Sized + Endpoint + RTArray + TimedEvent + Model + 'a,
+    T: Sized + Endpoint + RTArray + Model,
 {
-    fn call(&self, change: Change<'a, T>) -> impl Future + 'a;
+    type Fut: Future + Send;
+    fn call(&self, change: Change<'a, T>) -> Self::Fut;
 }
 
 impl<'a, T, Fut, Func> NestedListenerCallback<'a, T> for Func
 where
-    T: Sized + Endpoint + RTArray + TimedEvent + Model + 'a,
-    Fut: Future + 'a,
+    T: Sized + Endpoint + RTArray + Model + 'a,
+    Fut: Future + Send,
     Func: Fn(Change<'a, T>) -> Fut,
 {
-    fn call(&self, change: Change<'a, T>) -> impl Future + 'a {
+    type Fut = Fut;
+    fn call(&self, change: Change<'a, T>) -> Self::Fut {
         self(change)
     }
 }
@@ -48,38 +51,44 @@ where
 // --------- STATEFUL CALLBACKS
 pub trait StatefulListenerCallback<'a, T, S>
 where
-    T: Sized + Endpoint + RTObject + Model + 'a,
+    T: Sized + Endpoint + RTObject + Model,
     S: Sized + Send + Sync,
 {
-    fn call_with_state(&self, state: S, before: &'a T, after: &'a T) -> impl Future + 'a;
+    type Fut: Future + Send;
+    fn call_with_state(&self, state: S, before: &'a T, after: &'a T) -> Self::Fut;
 }
 
 impl<'a, T, Fut, Func, S> StatefulListenerCallback<'a, T, S> for Func
 where
     T: Sized + Endpoint + RTObject + Model + 'a,
     S: Sized + Send + Sync,
-    Fut: Future + 'a,
+    Fut: Future + Send,
     Func: Fn(S, &'a T, &'a T) -> Fut,
 {
-    fn call_with_state(&self, state: S, before: &'a T, after: &'a T) -> impl Future + 'a {
+    type Fut = Fut;
+    fn call_with_state(&self, state: S, before: &'a T, after: &'a T) -> Self::Fut {
         self(state, before, after)
     }
 }
 
 pub trait StatefulNestedListenerCallback<'a, T, S>
 where
-    T: Sized + Endpoint + RTArray + TimedEvent + Model + 'a,
+    T: Sized + Endpoint + RTArray + Model,
+    S: Sized + Send + Sync,
 {
-    fn call_with_state(&self, state: S, change: Change<'a, T>) -> impl Future + 'a;
+    type Fut: Future + Send;
+    fn call_with_state(&self, state: S, change: Change<'a, T>) -> Self::Fut;
 }
 
 impl<'a, T, Fut, Func, S> StatefulNestedListenerCallback<'a, T, S> for Func
 where
-    T: Sized + Endpoint + RTArray + TimedEvent + Model + 'a,
-    Fut: Future + 'a,
+    T: Sized + Endpoint + RTArray + Model + 'a,
+    S: Sized + Send + Sync,
+    Fut: Future + Send,
     Func: Fn(S, Change<'a, T>) -> Fut,
 {
-    fn call_with_state(&self, state: S, change: Change<'a, T>) -> impl Future + 'a {
+    type Fut = Fut;
+    fn call_with_state(&self, state: S, change: Change<'a, T>) -> Self::Fut {
         self(state, change)
     }
 }
@@ -114,5 +123,32 @@ where
             .filter(|&item| !self.incoming.contains(item))
             .map(Change::Added)
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use crate::worldstate::prelude::{Cetus, Fissure};
+
+    use super::Change;
+
+    async fn on_cetus_update(_before: &Cetus, _after: &Cetus) {}
+    async fn on_cetus_update_stateful_nested(_state: Arc<i32>, _change: Change<'_, Fissure>) {}
+
+    #[tokio::test]
+    async fn test() {
+        use crate::worldstate::prelude::*;
+        let client = Arc::new(Client::new());
+
+        let cloned = client.clone();
+        tokio::task::spawn(async move { cloned.call_on_update(on_cetus_update).await });
+        let cloned = client.clone();
+        tokio::task::spawn(async move {
+            cloned
+                .call_on_nested_update_with_state(on_cetus_update_stateful_nested, Arc::new(4))
+                .await
+        });
     }
 }
