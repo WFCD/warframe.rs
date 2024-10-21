9c49e9257e7ea6eaa02275372588724fdd23161d
Mettwasser <kaicrafter2000@gmail.com>
2024-10-21 16:49:16 +0200

    chore(cleanup): applied rename_all = lowercase instead of manually renaming every variant

--- src/worldstate/models/items/mod.rs
+++ src/worldstate/models/items/mod.rs
@@ -40,38 +40,31 @@ pub mod weapon;
 
 /// Represents a polarity
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, strum::Display)]
+#[serde(rename_all = "lowercase")]
 pub enum Polarity {
     /// V (Damage, Powers) - Commonly dropped by Grineer
-    #[serde(rename = "madurai")]
     Madurai,
 
     /// D (Defensive, Health, Armor) - Dropped by all factions
-    #[serde(rename = "vazarin")]
     Vazarin,
 
     /// Dash/Bar (Utility, Misc.) - Commonly dropped by Corpus
-    #[serde(rename = "naramon")]
     Naramon,
 
     /// Mainly used for Warframe Augment Mods, in addition to some Melee Stance Mods
-    #[serde(rename = "zenurik")]
     Zenurik,
 
     /// Used for certain Melee Stance Mods
-    #[serde(rename = "unairu")]
     Unairu,
 
     /// Y (Companion Abilities) - Dropped by all factions
-    #[serde(rename = "penjaga")]
     Penjaga,
 
     /// U (Anti-Sentient Mods) - Obtained upon completion of The Sacrifice
-    #[serde(rename = "umbra")]
     Umbra,
 
     /// O (Universal polarity) - Can be only applied by Aura- and Stance-Forma on their slots
     /// respectively
-    #[serde(rename = "any")]
     Any,
 }
 
