75c425cb7f399642226aa4a81704433b3da67a52
Mettwasser <kaicrafter2000@gmail.com>
2024-10-27 12:17:46 +0100

    feat: damage types and faction overhaul

---
+++ src/worldstate/models/damage_type.rs
@@ -0,0 +1,118 @@
+use serde::Deserialize;
+use serde_with::rust::deserialize_ignore_any;
+
+macro_rules! unordered_pattern {
+    ($a:pat, $b:pat) => {
+        ($a, $b) | ($b, $a)
+    };
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, derive_more::From, Hash)]
+#[serde(untagged)]
+pub enum DamageType {
+    // Physical Damage
+    Physical(PhysicalDamage),
+    // Primary Elemental Damage
+    Elemental(ElementalDamage),
+    // Secondary Elemental Damage
+    Combined(CombinedElementalDamage),
+
+    #[serde(deserialize_with = "deserialize_ignore_any")]
+    Other,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, derive_more::Display, Hash)]
+#[serde(rename_all = "lowercase")]
+pub enum PhysicalDamage {
+    Impact,
+    Puncture,
+    Slash,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, derive_more::Display, Hash)]
+#[serde(rename_all = "lowercase")]
+pub enum ElementalDamage {
+    Heat,
+    Cold,
+    Electricity,
+    Toxin,
+    Void,
+    Tau,
+}
+
+impl ElementalDamage {
+    /// Combines two Primary Elements into their Combined Element.
+    ///
+    /// Returns [None] if both `a` and `b` are equal.
+    pub const fn combine(a: Self, b: Self) -> Option<CombinedElementalDamage> {
+        use CombinedElementalDamage::*;
+        use ElementalDamage::*;
+
+        let combined_element = match (a, b) {
+            unordered_pattern!(Heat, Cold) => Blast,
+            unordered_pattern!(Heat, Electricity) => Radiation,
+            unordered_pattern!(Heat, Toxin) => Gas,
+            unordered_pattern!(Toxin, Cold) => Viral,
+            unordered_pattern!(Toxin, Electricity) => Corrosive,
+            unordered_pattern!(Cold, Electricity) => Magnetic,
+            _ => return None,
+        };
+
+        Some(combined_element)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, derive_more::Display, Hash)]
+#[serde(rename_all = "lowercase")]
+pub enum CombinedElementalDamage {
+    Radiation,
+    Blast,
+    Viral,
+    Corrosive,
+    Gas,
+    Magnetic,
+}
+
+impl CombinedElementalDamage {
+    /// Breaks down a combined element into both of its [PrimaryElement]s.
+    ///
+    /// The order in which they are returned follows classic HCET.
+    pub const fn break_down(self) -> (ElementalDamage, ElementalDamage) {
+        use CombinedElementalDamage::*;
+        use ElementalDamage::*;
+
+        match self {
+            Radiation => (Heat, Electricity),
+            Blast => (Heat, Cold),
+            Viral => (Cold, Toxin),
+            Corrosive => (Electricity, Toxin),
+            Gas => (Heat, Toxin),
+            Magnetic => (Cold, Electricity),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_combine() {
+        let cold = ElementalDamage::Cold;
+        let electricity = ElementalDamage::Electricity;
+
+        let combined: Option<CombinedElementalDamage> = ElementalDamage::combine(cold, electricity);
+
+        assert_eq!(combined.unwrap(), CombinedElementalDamage::Magnetic);
+    }
+
+    #[test]
+    fn test_break_down() {
+        let corrosive = CombinedElementalDamage::Corrosive;
+
+        assert_eq!(
+            corrosive.break_down(),
+            (ElementalDamage::Electricity, ElementalDamage::Toxin)
+        );
+    }
+}
