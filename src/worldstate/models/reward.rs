ca1dc60a7ee96f73bd63c0f422abf071b49cf403
Mettwasser <kaicrafter2000@gmail.com>
2025-09-20 15:12:10 +0200

    fix: api changes for rewards
    BREAKING CHANGE: removed as_string and item_string

--- src/worldstate/models/reward.rs
+++ src/worldstate/models/reward.rs
@@ -25,12 +25,6 @@ pub struct Reward {
     /// Amount of credits awarded
     pub credits: i32,
 
-    /// string representation of the reward
-    pub as_string: String,
-
     /// Items' names possible to be won
     pub items: Vec<String>,
-
-    /// formatted string describing all included items
-    pub item_string: String,
 }
